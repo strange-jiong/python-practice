e28feb327467d7f59a9dd394b5250904aae4c02b
jiong <447991103@qq.com>
2016-04-07 15:03:12 +0800

    add
    add answers

---
+++ 0016/Test.py
@@ -0,0 +1,18 @@
+__author__ = 'jiong'
+
+import xlwt
+
+with open('numbers.txt') as f:
+    content = f.read()
+    lists = eval(content)
+
+
+print lists
+file = xlwt.Workbook()
+table = file.add_sheet('Test', cell_overwrite_ok=True)
+
+for row in range(len(lists)):
+    for col in range(len(lists[row])):
+        table.write(row, col, lists[row][col])
+
+file.save('result.xls')
